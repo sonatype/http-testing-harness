59bc4172d88b841b6b8e56d7b3c8f58494e700b6
Benjamin Hanzelmann <ben@nabcos.de>
2010-10-11 13:58:33 +0200

    o Move Redirection Tests

--- async-http-client-suite/src/test/java/org/sonatype/tests/redirect/RedirectHttpTest.java
+++ async-http-client-suite/src/test/java/org/sonatype/tests/redirect/RedirectHttpTest.java
@@ -13,13 +13,114 @@ package org.sonatype.tests.redirect;
  * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
  */
 
-import org.sonatype.tests.jetty.server.suites.RedirectTestSuite;
+import static org.junit.Assert.*;
+
+import java.util.concurrent.ExecutionException;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.sonatype.tests.async.util.AsyncSuiteConfiguration;
+import org.sonatype.tests.jetty.runner.ConfigurationRunner;
+import org.sonatype.tests.jetty.server.behaviour.Pause;
+import org.sonatype.tests.jetty.server.behaviour.Redirect;
+import org.sonatype.tests.jetty.server.impl.JettyServerProvider;
+import org.sonatype.tests.server.api.ServerProvider;
+
+import com.ning.http.client.MaxRedirectException;
+import com.ning.http.client.Response;
 
 /**
  * @author Benjamin Hanzelmann
  */
+@RunWith( ConfigurationRunner.class )
 public class RedirectHttpTest
-    extends RedirectTestSuite
+    extends AsyncSuiteConfiguration
 {
+
+    @Test( expected = MaxRedirectException.class )
+    public void testTooManyRedirects()
+        throws Throwable
+    {
+        String url = url( "redirect", String.valueOf( client().getConfig().getMaxRedirects() + 1 ), "foo" );
+        try
+        {
+            executeGet( url );
+            fail( "expected error" );
+        }
+        catch ( ExecutionException e )
+        {
+            throw e.getCause();
+        }
+    }
+
+    /**
+     * Async Bug? Redirects == connection attempts? ("first connect is a redirection")
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void testMaxRedirects()
+        throws Exception
+    {
+        String url = url( "redirect", String.valueOf( client().getConfig().getMaxRedirects() ), "foo" );
+        Response response = executeGet( url );
+        assertEquals( 200, response.getStatusCode() );
+        assertEquals( "foo", response.getResponseBody() );
+    }
+
+    @Test
+    public void testRedirectAbsolute()
+        throws Exception
+    {
+        provider().addBehaviour( "/absolute/*", new Redirect( url( "content", "someContent" ) ) );
+        String url = url( "absolute", "foo" );
+        Response response = executeGet( url );
+        assertEquals( 200, response.getStatusCode() );
+        assertEquals( "someContent", response.getResponseBody() );
+    }
+
+    @Test
+    public void testRedirectOtherServer()
+        throws Exception
+    {
+        ServerProvider p = new JettyServerProvider();
+        p.start();
+
+        provider().addBehaviour( "/external/*", new Redirect( "http://localhost:" + p.getPort() + "/content/foo" ) );
+        Response response = executeGet( url( "external", "bar" ) );
+
+        assertEquals( 200, response.getStatusCode() );
+        assertEquals( "foo", response.getResponseBody() );
+    }
+
+    @Test
+    public void testRedirectToSSL()
+        throws Exception
+    {
+        ServerProvider p = new JettyServerProvider();
+        p.setSSL( "keystore", "password" );
+        p.start();
+
+        provider().addBehaviour( "/external/*", new Redirect( p.getUrl() + "/content/foo" ) );
+        Response response = executeGet( url( "external", "bar" ) );
+
+        assertEquals( 200, response.getStatusCode() );
+        assertEquals( "foo", response.getResponseBody() );
+    }
+
+    @Test
+    public void testTimeoutAfterRedirect()
+        throws Exception
+    {
+        ServerProvider p = new JettyServerProvider();
+        p.start();
+
+        provider().addBehaviour( "/external/*", new Redirect( "http://localhost:" + p.getPort() + "/content/foo" ),
+                                 new Pause( 10000 ) );
+        Response response = executeGet( url( "external", "bar" ) );
+
+        assertEquals( 200, response.getStatusCode() );
+        assertEquals( "foo", response.getResponseBody() );
+    }
 
 }
