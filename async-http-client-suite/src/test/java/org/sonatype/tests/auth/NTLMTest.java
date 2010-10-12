70fc3926ea3c336b87eee578456fbce119666d70
Benjamin Hanzelmann <ben@nabcos.de>
2010-10-12 12:58:22 +0200

    o start NTLM test

---
+++ async-http-client-suite/src/test/java/org/sonatype/tests/auth/NTLMTest.java
@@ -0,0 +1,60 @@
+package org.sonatype.tests.auth;
+
+/*
+ * Copyright (c) 2010 Sonatype, Inc. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0, 
+ * and you may not use this file except in compliance with the Apache License Version 2.0. 
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, 
+ * software distributed under the Apache License Version 2.0 is distributed on an 
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.sonatype.tests.async.util.AsyncSuiteConfiguration;
+import org.sonatype.tests.jetty.runner.ConfigurationRunner;
+import org.sonatype.tests.jetty.server.behaviour.Content;
+import org.sonatype.tests.jetty.server.behaviour.NTLMAuth;
+
+import com.ning.http.client.AsyncHttpClient;
+import com.ning.http.client.AsyncHttpClient.BoundRequestBuilder;
+import com.ning.http.client.Realm;
+import com.ning.http.client.Realm.AuthScheme;
+import com.ning.http.client.Response;
+import com.ning.http.client.providers.netty.NettyAsyncHttpProvider;
+
+/**
+ * @author Benjamin Hanzelmann
+ *
+ */
+@RunWith( ConfigurationRunner.class )
+public class NTLMTest
+    extends AsyncSuiteConfiguration
+{
+
+    @Test
+    public void testSimple()
+        throws Exception
+    {
+        provider().stop();
+        provider().addBehaviour( "/*", new NTLMAuth(), new Content() );
+        provider().start();
+
+        AsyncHttpClient c = new AsyncHttpClient( new NettyAsyncHttpProvider( builder().build() ), builder().build() );
+
+        BoundRequestBuilder rb = c.prepareGet( url( "content", "test" ) );
+        Realm realm =
+            new Realm.RealmBuilder().setDomain( "Test Server" ).setScheme( AuthScheme.NTLM ).setUsePreemptiveAuth( false ).build();
+        rb.setRealm( realm );
+        Response response = execute( rb );
+
+        assertEquals( "foo", response.getResponseBody() );
+    }
+
+}
