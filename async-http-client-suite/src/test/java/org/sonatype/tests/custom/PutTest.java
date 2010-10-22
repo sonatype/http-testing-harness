0c306aa421a4d961ec6617f3e4239f736b8b0e16
Benjamin Hanzelmann <ben@nabcos.de>
2010-10-22 22:16:25 +0200

    o Tweak ahc-test-suite

--- async-http-client-suite/src/test/java/org/sonatype/tests/custom/PutTest.java
+++ async-http-client-suite/src/test/java/org/sonatype/tests/custom/PutTest.java
@@ -16,22 +16,20 @@ package org.sonatype.tests.custom;
 import static org.junit.Assert.*;
 
 import java.io.File;
-import java.util.Map;
+import java.io.IOException;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import org.junit.AfterClass;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
-import org.sonatype.tests.async.util.AsyncDebugHandler;
 import org.sonatype.tests.async.util.AsyncSuiteConfiguration;
 import org.sonatype.tests.jetty.runner.ConfigurationRunner;
 import org.sonatype.tests.jetty.runner.ConfigurationRunner.ConfiguratorList;
 import org.sonatype.tests.jetty.server.behaviour.Consumer;
 import org.sonatype.tests.jetty.server.behaviour.ErrorBehaviour;
 import org.sonatype.tests.jetty.server.util.FileUtil;
+import org.sonatype.tests.server.api.ServerProvider;
 
 import com.ning.http.client.AsyncHttpClient;
 import com.ning.http.client.AsyncHttpClient.BoundRequestBuilder;
@@ -49,6 +47,19 @@ public class PutTest
 
     private static Logger logger = org.slf4j.LoggerFactory.getLogger( PutTest.class );
 
+    private static File largeFile;
+
+    private static long heapSize;
+
+
+    @AfterClass
+    public static void afterClass()
+        throws IOException
+    {
+        FileUtil.delete( largeFile );
+        largeFile = null;
+    }
+
     @Override
     public void before()
         throws Exception
@@ -58,90 +69,98 @@ public class PutTest
     }
 
     @Test
-    @Ignore("server side heap space problem")
+    @Ignore( "AHC error?" )
     public void testPutLargeFile()
         throws Exception
     {
-        File largeFile = null;
-        try
+        if ( largeFile == null )
         {
-            Consumer consumer = new Consumer();
-            provider().addBehaviour( "/consume/*", consumer );
-
-            String url = url( "consume", "foo" );
-            byte[] bytes = "RatherLargeFile".getBytes( "UTF-16" );
-            long heapSize = Runtime.getRuntime().maxMemory();
-            long repeats = ( heapSize / bytes.length ) + 1;
+            byte[] bytes = "RatherLargeFileRatherLargeFileRatherLargeFileRatherLargeFile".getBytes( "UTF-16" );
+            heapSize = Runtime.getRuntime().maxMemory();
             logger.debug( "creating file of size ~" + heapSize );
+            long repeats = ( heapSize / bytes.length ) + 1;
             largeFile = FileUtil.createTempFile( bytes, (int) repeats );
-
-            Builder cfg = new Builder();
-            cfg.setIdleConnectionTimeoutInMs( (int) heapSize );
-            cfg.setConnectionTimeoutInMs( (int) heapSize );
-            cfg.setRequestTimeoutInMs( (int) heapSize );
-            AsyncHttpClient c = new AsyncHttpClient( cfg.build() );
-
-            BoundRequestBuilder put = c.preparePut( url );
-            requestSettings( put );
-            put.setBody( largeFile );
-            put.execute( new AsyncDebugHandler() ).get();
-
-            assertEquals( largeFile.length(), consumer.getTotal() );
+            logger.debug( "created file of size " + largeFile.length() );
         }
-        finally
-        {
-            FileUtil.delete( largeFile );
-        }
+        Consumer consumer = new Consumer();
+        provider().addBehaviour( "/consume/*", consumer );
+
+        String url = url( "consume", "foo" );
+
+        Builder cfg = new Builder();
+        cfg.setIdleConnectionTimeoutInMs( (int) heapSize );
+        cfg.setConnectionTimeoutInMs( (int) heapSize );
+        cfg.setRequestTimeoutInMs( (int) heapSize );
+        AsyncHttpClient c = new AsyncHttpClient( cfg.build() );
+
+        BoundRequestBuilder put = c.preparePut( url );
+        put.setBody( largeFile );
+
+        execute( put );
+
+        assertEquals( largeFile.length(), consumer.getTotal() );
 
     }
 
     @Test
-    public void testPutMethodNotSupported()
+    public void testPutFile()
         throws Exception
     {
-        provider().addBehaviour( "/methodnotsupported/*", new ErrorBehaviour( 501, "errormsg" )
+        Consumer consumer = new Consumer();
+        provider().addBehaviour( "/testPutFile/*", consumer );
+
+        String url = url( "testPutFile", "foo" );
+
+        BoundRequestBuilder put = client().preparePut( url );
+        File f = FileUtil.createTempFile( "This is a file." );
+        put.setBody( f );
+        execute( put );
+
+        try
         {
+            assertEquals( f.length(), consumer.getTotal() );
+        }
+        finally
+        {
+            FileUtil.delete( f );
+        }
+    }
 
-            @Override
-            public boolean execute( HttpServletRequest request, HttpServletResponse response, Map<Object, Object> ctx )
-                throws Exception
-            {
-                response.setHeader( "Allow", "GET, HEAD, POST" );
-                return super.execute( request, response, ctx );
-            }
-
-        } );
-
-        String url = url( "methodnotsupport", "error" );
+    @Test
+    public void testPutBytes()
+        throws Exception
+    {
+        Consumer consumer = new Consumer();
+        provider().addBehaviour( "/put/*", consumer );
+        String url = url( "put", "someData" );
         BoundRequestBuilder rb = client().preparePut( url );
+        byte[] bytes = "datavalue".getBytes( "UTF-8" );
+        rb.setBody( bytes );
+
         Response response = execute( rb );
 
-        assertEquals( 405, response.getStatusCode() );
+        assertEquals( 200, response.getStatusCode() );
+        assertEquals( bytes.length, consumer.getTotal() );
     }
 
     @Test
     public void testPutError()
         throws Exception
     {
-        provider().addBehaviour( "/methodsupported/*", new ErrorBehaviour( 404, "errormsg" )
-        {
 
-            @Override
-            public boolean execute( HttpServletRequest request, HttpServletResponse response, Map<Object, Object> ctx )
-                throws Exception
-            {
-                response.setHeader( "Allow", "GET, HEAD, POST, PUT" );
-                return super.execute( request, response, ctx );
-            }
-
-        } );
-        String url = url( "methodsupported", "404", "errormsg" );
+        String url = url( "methodsupported", "501", "errormsg" );
         BoundRequestBuilder rb = client().preparePut( url );
         Response response = execute( rb );
 
-        assertEquals( 404, response.getStatusCode() );
+        assertEquals( 501, response.getStatusCode() );
         assertEquals( "errormsg", response.getStatusText() );
     }
 
+    @Override
+    public void configureProvider( ServerProvider provider )
+    {
+        super.configureProvider( provider );
+        provider.addBehaviour( "/methodsupported/*", new ErrorBehaviour( 501, "errormsg" ) );
+    }
 
 }
