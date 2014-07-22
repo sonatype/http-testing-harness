793e9fd27dcda8311a3e5204a401f8aa57c8efb1
Jason Dillon <jason@planet57.com>
2014-07-21 18:19:34 -0700

    Update license headers

--- server-provider/src/main/java/org/sonatype/tests/http/server/api/Behaviour.java
+++ server-provider/src/main/java/org/sonatype/tests/http/server/api/Behaviour.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2013 Sonatype, Inc. All rights reserved.
+ * Copyright (c) 2010-2014 Sonatype, Inc. All rights reserved.
  *
  * This program is licensed to you under the Apache License Version 2.0,
  * and you may not use this file except in compliance with the Apache License Version 2.0.
@@ -10,7 +10,6 @@
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
  */
-
 package org.sonatype.tests.http.server.api;
 
 import java.util.Map;
